73b288dc01f03d4ce4941a9c885cad1de23082ab
Neagu Alexandru <neagu.alex0709@gmail.com>
2024-12-03 11:15:03 +0100

    aggiunto tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jpg,png}',
+    './src/img/**/*.{html,js,jpg,png,svg}',
+    './src/scripts/**/*.js',
+  ],
+  theme: {
+    extend: {
+
+    },
+  },
+  plugins: [
+
+  ],
+}
+
